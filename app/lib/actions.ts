bda2e776d21325b8afdc9ae25be12ca0c1334357
Mateus Santos <m.santos.azeve@gmail.com>
2024-05-14 16:54:44 -0300

    feat: validate and prepare invoice form data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,11 +1,27 @@
 'use server';
 
+import { z } from 'zod';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  data: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { amount, customerId, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
-  // Test it out:
-  console.log(rawFormData);
-}
+  });
+  /**
+   * It's a good practice to store monetary values in cents in your database to
+   * eliminate JavaScript floating-point errors and ensure greater accuracy.
+   */
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+};
