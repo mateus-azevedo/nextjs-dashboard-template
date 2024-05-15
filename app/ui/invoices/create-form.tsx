e9e267b028cef86f7092c38143b4869939a5d161
Mateus Santos <m.santos.azeve@gmail.com>
2024-05-15 17:37:49 -0300

    docs: add commentary about error props

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -15,7 +15,23 @@ import { createInvoice } from '@/app/lib/actions';
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState = { message: null, errors: {} };
   const [state, dispatch] = useFormState(createInvoice, initialState);
-  
+
+  /**
+    * aria-describedby="customer-error": This establishes a relationship between
+      the select element and the error message container. It indicates that the
+      container with id="customer-error" describes the select element. Screen
+      readers will read this description when the user interacts with the select
+      box to notify them of errors.
+
+    * id="customer-error": This id attribute uniquely identifies the HTML
+      element that holds the error message for the select input. This is
+      necessary for aria-describedby to establish the relationship.
+
+    * aria-live="polite": The screen reader should politely notify the user when
+      the error inside the div is updated. When the content changes (e.g. when a
+      user corrects an error), the screen reader will announce these changes,
+      but only when the user is idle so as not to interrupt them.
+   */
   return (
     <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
